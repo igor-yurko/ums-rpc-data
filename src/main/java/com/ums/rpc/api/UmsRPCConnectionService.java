66e752c83ed115dd6f88a172d92ba373ad194e2d
Igor Yurko <igor.yurko.mail@gmail.com>
2019-11-08 17:58:53 +0200

    Add system Income method.

--- src/main/java/com/ums/rpc/api/UmsRPCConnectionService.java
+++ src/main/java/com/ums/rpc/api/UmsRPCConnectionService.java
@@ -4,9 +4,13 @@ import com.ums.rpc.data.CreditedAccrualDto;
 import com.ums.rpc.data.TokensDto;
 import com.ums.rpc.data.UserDto;
 
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
 public interface UmsRPCConnectionService {
 	Boolean addAccrualAmountToWallet(CreditedAccrualDto accrual);
 	Boolean isOrderPaid(String orderUuid);
 	TokensDto setBaseUserRole(String productApiKey, String userUuid, String role);
 	UserDto getUserProfileData(String userUuid);
+	BigDecimal getSystemIncomePerPeriod(ZonedDateTime startTime, ZonedDateTime endTime);
 }
